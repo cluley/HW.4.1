99a393ab4f46c75838a55ab5eb8c67d4ed2aa426
Cluley <oo43@yandex.ru>
2023-02-03 21:31:12 +0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+
+class Adress {
+public:
+	void set(std::ifstream& input);
+	void get(std::ofstream& output);
+private:
+	std::string city;
+	std::string strt;
+	int bldng;
+	int aptmt;
+};
+
+void Adress::set(std::ifstream& input) { 
+	input >> city >> strt >> bldng >> aptmt; 
+}
+
+void Adress::get(std::ofstream& output) {
+	output << city << ", " << strt << ", " << bldng << ", " << aptmt << '\n';
+}
+
+int main(int argc, char** argv) {
+	int total;
+	std::ifstream input("in.txt");
+	input >> total;
+
+	Adress* adresses = new Adress[total];
+
+	for (int i = 0; i < total; i++) {
+		adresses[i].set(input);
+	}
+
+	std::ofstream output("out.txt");
+	output << total << '\n'; 
+
+	for (int i = (total - 1); i > -1; i--) {
+		adresses[i].get(output);
+	}
+
+	return 0;
+}
